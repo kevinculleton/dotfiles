a70f180b579343c921407fcd957f9d7b2782875d
carmac <kpc@live.ie>
2023-12-20 22:55:10 +0000

    layout update

--- .tmux/plugins/tmuxifier/layouts/dev.session.sh
+++ .tmux/plugins/tmuxifier/layouts/dev.session.sh
@@ -1,14 +1,19 @@
 # Set a custom session root path. Default is `$HOME`.
 # Must be called before `initialize_session`.
-session_root "~/notes"
+session_root "~/projects"
 
 # Create session with specified name if it does not already exist. If no
 # argument is given, session name will be based on layout file name.
-if initialize_session "notes"; then
+if initialize_session "dev"; then
 
 	# Create a new window inline within session layout definition.
-	new_window "misc"
+	new_window "code"
 	run_cmd "v ."
+
+	new_window "terminal"
+
+	# Select the default active window on session creation.
+	select_window 1
 
 fi
 
