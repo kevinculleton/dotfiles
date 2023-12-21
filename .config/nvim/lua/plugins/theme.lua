9df77539e620eba9d7d55555c2f3740caef6da5c
carmac <kpc@live.ie>
2023-12-21 19:49:33 +0000

    update nvim theme

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,9 +1,10 @@
 return {
-  { "ellisonleao/gruvbox.nvim" },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
-  },
+  "rockyzhang24/arctic.nvim",
+  dependencies = { "rktjmp/lush.nvim" },
+  name = "arctic",
+  branch = "main",
+  priority = 1000,
+  config = function()
+    vim.cmd("colorscheme arctic")
+  end,
 }
